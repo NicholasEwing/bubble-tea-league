819bbd221b240f77f23264de32c9289a92a69199
Nicholas Ewing <nicholasSewing@gmail.com>
2022-01-25 10:09:15 -0500

    Fixes default logo color

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function loadNavbar() {
   <header class="text-gray-400 bg-gray-900 body-font">
   <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-red-500 rounded-full" viewBox="0 0 24 24">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
       <a href="" class="text-white text-xl font-medium hover:text-gray-300"><span class="ml-3 text-xl">Bubble Tea League Stats Tracker</span></a>
