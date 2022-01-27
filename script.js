2b68a31df63d6efbf727e1a98a6835674317abd0
Nicholas Ewing <nicholasSewing@gmail.com>
2022-01-27 18:56:23 -0500

    Correct navbar site title

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function loadNavbar() {
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
-      <a href="./index.html" class="text-white text-xl font-medium hover:text-gray-300"><span class="ml-3 text-xl">Bubble Tea League Stats Tracker</span></a>
+      <a href="./index.html" class="text-white text-xl font-medium hover:text-gray-300"><span class="ml-3 text-xl">Bubble Tea League</span></a>
     </a>
     <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
       <a href="./matches.html"class="mr-5 hover:text-white">Matches</a>
