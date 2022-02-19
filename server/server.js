47d2ee44baf060f2713c9f90b2640dc916226095
Nicholas Ewing <nicholasSewing@gmail.com>
2022-02-19 13:24:29 -0500

    Establishes basic unsecure connection to MySQL RDS

--- server/server.js
+++ server/server.js
@@ -1,3 +1,29 @@
-const { saveFile, teamDistFilename, templatizedPage } = require("./staticGen");
+require("dotenv").config();
+const Sequelize = require("sequelize");
 
-saveFile(teamDistFilename, templatizedPage);
+// connect to aws database
+const sequelize = new Sequelize(
+  "bubble_tea_league",
+  process.env.USERNAME,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    port: 3306,
+    dialect: "mysql",
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connected!");
+  })
+  .catch((error) => console.log(error));
+
+// add a team to the db
+
+// pull team name from the db, make a team
+
+// add 5 teams to the db
+
+// write logic to create all 5 teams, organize the staticGen logic into a class for each template
