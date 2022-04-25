d085fa2379eecce1c90e796e4d5d53850d87806f
Nicholas Ewing <nicholasSewing@gmail.com>
2022-04-25 13:47:28 -0400

    Adds error handling to /api/teams

--- src/express/routes/teams.js
+++ src/express/routes/teams.js
@@ -11,56 +11,68 @@ async function getAll(req, res) {
 }
 
 async function getById(req, res) {
-  const id = getIdParam(req);
-  const team = await models.Team.findByPk(id);
-  if (team) {
+  try {
+    const id = getIdParam(req);
+    const team = await models.Team.findByPk(id);
     res.status(200).json(team);
-  } else {
-    res.status(404).send("404 - Not found");
+  } catch (error) {
+    res.status(404).send(error);
   }
 }
 
 async function create(req, res) {
-  if (req.body.id) {
-    res
-      .status(400)
-      .send(
-        `Bad request: ID should not be provided, since it is determined automatically by the database.`
-      );
-  } else {
-    await models.Team.create(req.body);
-    res.status(201).end();
+  try {
+    if (req.body.id) {
+      res
+        .status(400)
+        .send(
+          `Bad request: ID should not be provided, since it is determined automatically by the database.`
+        );
+    } else {
+      await models.Team.create(req.body);
+      res.status(201).end();
+    }
+  } catch (error) {
+    res.status(404).send(error);
   }
 }
 
 async function update(req, res) {
-  const id = getIdParam(req);
+  try {
+    const id = getIdParam(req);
 
-  // We only accept an UPDATE request if the `:id` param matches the body `id`
-  if (req.body.id === id) {
-    await models.Team.update(req.body, {
+    // We only accept an UPDATE request if the `:id` param matches the body `id`
+    if (req.body.id === id) {
+      await models.Team.update(req.body, {
+        where: {
+          id: id,
+        },
+      });
+      res.status(200).end();
+    } else {
+      res
+        .status(400)
+        .send(
+          `Bad request: param ID (${id}) does not match body ID (${req.body.id}).`
+        );
+    }
+  } catch (error) {
+    res.status(404).send(error);
+  }
+}
+
+async function remove(req, res) {
+  try {
+    const id = getIdParam(req);
+    await models.Team.destroy({
       where: {
         id: id,
       },
     });
     res.status(200).end();
-  } else {
-    res
-      .status(400)
-      .send(
-        `Bad request: param ID (${id}) does not match body ID (${req.body.id}).`
-      );
+  } catch (error) {
+    res.status(404).send(error);
   }
-}
-
-async function remove(req, res) {
-  const id = getIdParam(req);
-  await models.Team.destroy({
-    where: {
-      id: id,
-    },
-  });
-  res.status(200).end();
 }
 
 module.exports = {
