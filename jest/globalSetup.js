6ca9a7fa75527c8919c1e3a1783b2105e544a00e
Nicholas Ewing <nicholasSewing@gmail.com>
2022-06-10 17:11:55 -0400

    Removes dotenv, uses native nextjs env

--- jest/globalSetup.js
+++ jest/globalSetup.js
@@ -1,4 +1,5 @@
 const sequelize = require("../sequelize/index");
+import { loadEnvConfig } from "@next/env";
 
 const makeTables = async () => {
   await sequelize.sync({ force: true });
@@ -7,6 +8,8 @@ const makeTables = async () => {
 
 const globalSetup = async () => {
   try {
+    const projectDir = process.cwd();
+    loadEnvConfig(projectDir);
     await makeTables();
   } catch (error) {
     console.error("Global setup error:", error);
