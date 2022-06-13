db2bf8ca05f7821c9b588ecb2788315640fed3c3
Nicholas Ewing <nicholasSewing@gmail.com>
2022-06-13 15:37:20 -0400

    Accepts my fate with babel, gets basic discord auth and tests working again

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,14 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
+import SequelizeAdapter from "@next-auth/sequelize-adapter";
+import sequelize from "../../../sequelize/index.js";
 
 export default NextAuth({
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
     }),
+    // ...add more providers here
   ],
 });
