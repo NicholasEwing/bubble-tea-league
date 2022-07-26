77a85c7e7156faba915c735f005eb835ab182000
Nicholas Ewing <nicholasSewing@gmail.com>
2022-07-26 19:46:30 -0400

    adds fallback for team logo

--- components/match-results/TeamHeader/TeamLogo.js
+++ components/match-results/TeamHeader/TeamLogo.js
@@ -1,17 +1,23 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 export default function TeamLogo({
   tricode = "null",
   width = 36,
   height = 36,
+  faded,
 }) {
+  const [src, setSrc] = useState(`/teams/${tricode}.png`);
+
   return (
-    <Image
-      src={`/teams/${tricode}.png`}
-      alt="Team Logo"
-      width={width}
-      height={height}
-    />
+    <div className={`flex ${faded && "opacity-30"}`}>
+      <Image
+        onError={() => setSrc("/teams/null.png")}
+        src={src}
+        alt="Team Logo"
+        width={width}
+        height={height}
+      />
+    </div>
   );
 }
