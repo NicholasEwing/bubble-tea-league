049d3b77555df08b9f98436568d9eba6dc5ca5b6
Nicholas Ewing <nicholasSewing@gmail.com>
2022-08-05 15:13:07 -0400

    makes TeamSection and let Teams be bulk updated via api

---
+++ components/admin/TeamsSection.js/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import EditableTable from "../EditableTable";
+import SeasonsTable from "../SeasonsTable";
+import AddButton from "../table/AddButton";
+import SectionContainer from "../table/SectionContainer";
+import TextHeadingContainer from "../TextHeadingContainer";
+
+export default function TeamsSection({ items }) {
+  const [open, setOpen] = useState(false);
+
+  const openModal = () => setOpen(true);
+  const closeModal = () => setOpen(false);
+
+  const teamColumns = [
+    {
+      valueKey: "id",
+      name: "id",
+    },
+    {
+      valueKey: "teamName",
+      name: "Team Name",
+      canEdit: true,
+    },
+    {
+      valueKey: "tricode",
+      name: "Tricode",
+      canEdit: true,
+    },
+    {
+      valueKey: "season",
+      name: "Season",
+    },
+  ];
+
+  // todo:
+  // make edit controls per ROW
+  // adjust input width to text or something
+  // tricode pattern enforcement
+  // team name length enforcement
+
+  return (
+    <SectionContainer>
+      <TextHeadingContainer>
+        <div className="flex justify-between">
+          <h1 className="text-xl font-semibold text-white">Teams</h1>
+          <AddButton buttonText="Add team" buttonAction={openModal} />
+        </div>
+        <p className="mt-2 text-sm text-gray-400">
+          A list of all the teams in the Bubble Tea League including their team
+          name, tricode, and season.
+        </p>
+      </TextHeadingContainer>
+      <EditableTable
+        items={items}
+        columns={teamColumns}
+        tableName="teams"
+        bulkEdit
+      />
+    </SectionContainer>
+  );
+}
--- pages/api/teams/index.js
+++ pages/api/teams/index.js
@@ -8,13 +8,24 @@ export default async function handler(req, res) {
         const teams = await Team.findAll();
         res.status(200).json(teams);
         break;
-      case "POST":
-        const team = await Team.create(req.body);
-        res.status(201).end();
-        break;
+      // case "POST":
+      //   const team = await Team.create(req.body);
+      //   res.status(201).end();
+      //   break;
       case "UPDATE":
         await Team.create(req.body);
         res.status(201).end();
+        break;
+      case "PATCH":
+        try {
+          const { teams } = req.body;
+          await Team.bulkCreate(teams, {
+            updateOnDuplicate: ["teamName", "tricode"],
+          });
+          res.status(200).send();
+        } catch (error) {
+          res.status(500).send({ error });
+        }
         break;
     }
   } catch (error) {
