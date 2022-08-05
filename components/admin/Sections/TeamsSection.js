3c08abceec61a1117e388e9acac8505a30a38ab2
Nicholas Ewing <nicholasSewing@gmail.com>
2022-08-05 19:08:35 -0400

    Allow commas in team name so default info doesn't stop you from typing

--- components/admin/Sections/TeamsSection.js
+++ components/admin/Sections/TeamsSection.js
@@ -20,7 +20,7 @@ export default function TeamsSection({ items }) {
       valueKey: "teamName",
       name: "Team Name",
       canEdit: true,
-      pattern: "^[a-zA-Z0-9._ \t]{1,255}$",
+      pattern: "^[a-zA-Z0-9,._ ]{1,255}$",
     },
     {
       valueKey: "tricode",
@@ -39,9 +39,7 @@ export default function TeamsSection({ items }) {
   return (
     <SectionContainer>
       <TextHeadingContainer>
-        <div className="flex justify-between">
-          <h1 className="text-xl font-semibold text-white">Teams</h1>
-        </div>
+        <h1 className="text-xl font-semibold text-white">Teams</h1>
         <p className="mt-2 text-sm text-gray-400">
           A list of all the teams in the Bubble Tea League including their team
           name, tricode, and season.
