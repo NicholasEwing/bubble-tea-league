521cd02735911bd86d61be691158caab9139f55d
Nicholas Ewing <nicholasSewing@gmail.com>
2022-08-10 14:12:41 -0400

    Pass custom error message to <Failed> component

--- components/alerts/Failed.js
+++ components/alerts/Failed.js
@@ -3,16 +3,14 @@ import { XCircleIcon } from "@heroicons/react/solid";
 import AlertContainer from "./AlertContainer";
 import XCloseAlert from "./XCloseAlert";
 
-export default function Failed({ closeError }) {
+export default function Failed({ closeError, errorMsg }) {
   return (
     <AlertContainer bgColor="red-50">
       <div className="flex-shrink-0">
         <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
       </div>
       <div className="ml-3 flex">
-        <h3 className="text-sm font-medium text-red-800">
-          Failed to apply changes.
-        </h3>
+        <h3 className="text-sm font-medium text-red-800">{errorMsg}</h3>
         <XCloseAlert success={false} dismissHandler={closeError} />
       </div>
     </AlertContainer>
