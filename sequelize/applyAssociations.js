c469653e661c4baeeb84d9835ba4f9317ee31015
Nicholas Ewing <nicholasSewing@gmail.com>
2022-09-01 13:24:25 -0400

    Moves constraint false to child

--- sequelize/applyAssociations.js
+++ sequelize/applyAssociations.js
@@ -11,48 +11,49 @@ const applyAssociations = (sequelize) => {
   } = sequelize.models;
 
   // Associate matches with seasons
-  Season.hasMany(Match, { foreignKey: "season", constraints: false });
-  Match.belongsTo(Season, { foreignKey: "season" });
+  Season.hasMany(Match, { foreignKey: "season" });
+  Match.belongsTo(Season, { foreignKey: "season", constraints: false });
 
   // Associate teams with seasons
-  Season.hasMany(Team, { foreignKey: "season", constraints: false });
-  Team.belongsTo(Season, { foreignKey: "season" });
+  Season.hasMany(Team, { foreignKey: "season" });
+  Team.belongsTo(Season, { foreignKey: "season", constraints: false });
 
   // Associate matches with match rounds (ex: A best of three match can have up to three rounds)
   Match.hasMany(MatchRound, { constraints: false });
   MatchRound.belongsTo(Match);
 
   // Associate matches with match winning teams
-  Team.hasMany(Match, { foreignKey: "matchWinnerTeamId", constraints: false });
+  Team.hasMany(Match, { foreignKey: "matchWinnerTeamId" });
   Match.belongsTo(Team, {
     foreignKey: "matchWinnerTeamId",
+    constraints: false,
   });
 
-  Team.hasMany(Match, { foreignKey: "matchLoserTeamId", constraints: false });
-  Match.belongsTo(Team, { foreignKey: "matchLoserTeamId" });
+  Team.hasMany(Match, { foreignKey: "matchLoserTeamId" });
+  Match.belongsTo(Team, { foreignKey: "matchLoserTeamId", constraints: false });
 
-  Team.hasMany(Match, { foreignKey: "teamOne", constraints: false });
-  Match.belongsTo(Team, { foreignKey: "teamTwo" });
+  Team.hasMany(Match, { foreignKey: "teamOne" });
+  Match.belongsTo(Team, { foreignKey: "teamTwo", constraints: false });
 
   // Keep track of seasonal team standings
-  Team.hasOne(TeamStanding, { constraints: false });
-  TeamStanding.belongsTo(Team);
+  Team.hasOne(TeamStanding);
+  TeamStanding.belongsTo(Team, { constraints: false });
 
   // Associate winning / losing teams with match rounds
   Team.hasMany(MatchRound, {
     foreignKey: "winningTeamId",
-    constraints: false,
   });
   MatchRound.belongsTo(Team, {
     foreignKey: "winningTeamId",
+    constraints: false,
   });
 
   Team.hasMany(MatchRound, {
     foreignKey: "redTeamId",
-    constraints: false,
   });
   MatchRound.belongsTo(Team, {
     foreignKey: "blueTeamId",
+    constraints: false,
   });
 
   // Associate teams with players per season (team has one season, player can have a new team per season)
