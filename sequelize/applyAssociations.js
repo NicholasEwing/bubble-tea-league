f9c5040e5787d8cc8a42fd8b98153a37841963c7
Nicholas Ewing <nicholasSewing@gmail.com>
2022-09-02 13:14:22 -0400

    Comments out previous association / connection files for now

--- sequelize/applyAssociations.js
+++ sequelize/applyAssociations.js
@@ -1,78 +1,78 @@
-const applyAssociations = (sequelize) => {
-  const {
-    Team,
-    Player,
-    Season,
-    Match,
-    MatchRound,
-    MatchRoundTeamStats,
-    MatchRoundPlayerStats,
-    TeamStanding,
-  } = sequelize.models;
+// const applyAssociations = (sequelize) => {
+//   const {
+//     Team,
+//     Player,
+//     Season,
+//     Match,
+//     MatchRound,
+//     MatchRoundTeamStats,
+//     MatchRoundPlayerStats,
+//     TeamStanding,
+//   } = sequelize.models;
 
-  // Associate matches with seasons
-  Season.hasMany(Match, { foreignKey: "season" });
-  Match.belongsTo(Season, { foreignKey: "season", constraints: false });
+//   // Associate matches with seasons
+//   Season.hasMany(Match, { foreignKey: "season" });
+//   Match.belongsTo(Season, { foreignKey: "season", constraints: false });
 
-  // Associate teams with seasons
-  Season.hasMany(Team, { foreignKey: "season" });
-  Team.belongsTo(Season, { foreignKey: "season", constraints: false });
+//   // Associate teams with seasons
+//   Season.hasMany(Team, { foreignKey: "season" });
+//   Team.belongsTo(Season, { foreignKey: "season", constraints: false });
 
-  // Associate matches with match rounds (ex: A best of three match can have up to three rounds)
-  Match.hasMany(MatchRound);
-  MatchRound.belongsTo(Match, { constraints: false });
+//   // Associate matches with match rounds (ex: A best of three match can have up to three rounds)
+//   Match.hasMany(MatchRound);
+//   MatchRound.belongsTo(Match, { constraints: false });
 
-  // Associate matches with match winning teams
-  Team.hasMany(Match, { foreignKey: "matchWinnerTeamId" });
-  Match.belongsTo(Team, {
-    foreignKey: "matchWinnerTeamId",
-    constraints: false,
-  });
+//   // Associate matches with match winning teams
+//   Team.hasMany(Match, { foreignKey: "matchWinnerTeamId" });
+//   Match.belongsTo(Team, {
+//     foreignKey: "matchWinnerTeamId",
+//     constraints: false,
+//   });
 
-  Team.hasMany(Match, { foreignKey: "matchLoserTeamId" });
-  Match.belongsTo(Team, { foreignKey: "matchLoserTeamId", constraints: false });
+//   Team.hasMany(Match, { foreignKey: "matchLoserTeamId" });
+//   Match.belongsTo(Team, { foreignKey: "matchLoserTeamId", constraints: false });
 
-  Team.hasMany(Match, { foreignKey: "teamOne" });
-  Match.belongsTo(Team, { foreignKey: "teamTwo", constraints: false });
+//   Team.hasMany(Match, { foreignKey: "teamOne" });
+//   Match.belongsTo(Team, { foreignKey: "teamTwo", constraints: false });
 
-  // Keep track of seasonal team standings
-  Team.hasOne(TeamStanding);
-  TeamStanding.belongsTo(Team, { constraints: false });
+//   // Keep track of seasonal team standings
+//   Team.hasOne(TeamStanding);
+//   TeamStanding.belongsTo(Team, { constraints: false });
 
-  // Associate winning / losing teams with match rounds
-  Team.hasMany(MatchRound, {
-    foreignKey: "winningTeamId",
-  });
-  MatchRound.belongsTo(Team, {
-    foreignKey: "winningTeamId",
-    constraints: false,
-  });
+//   // Associate winning / losing teams with match rounds
+//   Team.hasMany(MatchRound, {
+//     foreignKey: "winningTeamId",
+//   });
+//   MatchRound.belongsTo(Team, {
+//     foreignKey: "winningTeamId",
+//     constraints: false,
+//   });
 
-  Team.hasMany(MatchRound, {
-    foreignKey: "redTeamId",
-  });
-  MatchRound.belongsTo(Team, {
-    foreignKey: "blueTeamId",
-    constraints: false,
-  });
+//   Team.hasMany(MatchRound, {
+//     foreignKey: "redTeamId",
+//   });
+//   MatchRound.belongsTo(Team, {
+//     foreignKey: "blueTeamId",
+//     constraints: false,
+//   });
 
-  // Associate teams with players per season (team has one season, player can have a new team per season)
-  Team.belongsToMany(Player, {
-    through: "PlayerTeamHistory",
-    contraints: false,
-  });
-  Player.belongsToMany(Team, {
-    through: "PlayerTeamHistory",
-    constraints: false,
-  });
+//   // Associate teams with players per season (team has one season, player can have a new team per season)
+//   Team.belongsToMany(Player, {
+//     through: "PlayerTeamHistory",
+//     contraints: false,
+//   });
+//   Player.belongsToMany(Team, {
+//     through: "PlayerTeamHistory",
+//     constraints: false,
+//   });
 
-  // Connect team stats to a match round AND a team
-  MatchRoundTeamStats.belongsTo(MatchRound, { constraints: false });
-  MatchRoundTeamStats.belongsTo(Team, { constraints: false });
+//   // Connect team stats to a match round AND a team
+//   MatchRoundTeamStats.belongsTo(MatchRound, { constraints: false });
+//   MatchRoundTeamStats.belongsTo(Team, { constraints: false });
 
-  // Connect player stats to a match round AND a player
-  MatchRoundPlayerStats.belongsTo(MatchRound, { constraints: false });
-  MatchRoundPlayerStats.belongsTo(Player, { constraints: false });
-};
+//   // Connect player stats to a match round AND a player
+//   MatchRoundPlayerStats.belongsTo(MatchRound, { constraints: false });
+//   MatchRoundPlayerStats.belongsTo(Player, { constraints: false });
+// };
 
-module.exports = applyAssociations;
+// module.exports = applyAssociations;
--- sequelize/index.js
+++ sequelize/index.js
@@ -1,59 +1,59 @@
-const Sequelize = require("sequelize");
-const applyAssociations = require("./applyAssociations");
-const config = require("./config/config");
+// const Sequelize = require("sequelize");
+// const applyAssociations = require("./applyAssociations");
+// const config = require("./config/config");
 
-let sequelize,
-  database,
-  username,
-  password,
-  host,
-  port,
-  dialect,
-  dialectOptions;
+// let sequelize,
+//   database,
+//   username,
+//   password,
+//   host,
+//   port,
+//   dialect,
+//   dialectOptions;
 
-if (process.env.NODE_ENV === "test") {
-  ({ database, username, password, host, port, dialect } = config.test);
-} else if (process.env.NODE_ENV === "development") {
-  ({ database, username, password, host, port, dialect } = config.development);
-} else if (process.env.NODE_ENV === "production") {
-  const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING);
-}
+// if (process.env.NODE_ENV === "test") {
+//   ({ database, username, password, host, port, dialect } = config.test);
+// } else if (process.env.NODE_ENV === "development") {
+//   ({ database, username, password, host, port, dialect } = config.development);
+// } else if (process.env.NODE_ENV === "production") {
+//   const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING);
+// }
 
-if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development")
-  sequelize = new Sequelize(database, username, password, {
-    host,
-    port,
-    dialect,
-    // query: { raw: true }, // this breaks next-auth lol
-  });
+// if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development")
+//   sequelize = new Sequelize(database, username, password, {
+//     host,
+//     port,
+//     dialect,
+//     // query: { raw: true }, // this breaks next-auth lol
+//   });
 
-// Define all models and then attaches them to sequelize.models
-const modelDefiners = [
-  require("./models/team"),
-  require("./models/provider"),
-  require("./models/season"),
-  require("./models/player"),
-  require("./models/match"),
-  require("./models/matchRound"),
-  require("./models/matchRoundTeamStats"),
-  require("./models/matchRoundPlayerStats"),
-  require("./models/playerTeamHistory"),
-  require("./models/teamStanding"),
-];
+// // Define all models and then attaches them to sequelize.models
+// const modelDefiners = [
+//   require("./models/team"),
+//   require("./models/provider"),
+//   require("./models/season"),
+//   require("./models/player"),
+//   require("./models/match"),
+//   require("./models/matchRound"),
+//   require("./models/matchRoundTeamStats"),
+//   require("./models/matchRoundPlayerStats"),
+//   require("./models/playerTeamHistory"),
+//   require("./models/teamStanding"),
+// ];
 
-for (const modelDefiner of modelDefiners) {
-  modelDefiner(sequelize);
-}
+// for (const modelDefiner of modelDefiners) {
+//   modelDefiner(sequelize);
+// }
 
-applyAssociations(sequelize);
+// applyAssociations(sequelize);
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.log("Unable to connect to the database:", err);
-  });
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log("Connection has been established successfully.");
+//   })
+//   .catch((err) => {
+//     console.log("Unable to connect to the database:", err);
+//   });
 
-module.exports = sequelize;
+// module.exports = sequelize;
