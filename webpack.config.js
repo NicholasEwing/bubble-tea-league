389759ac9a2519172536bb2879014fe95dd15805
Nicholas Ewing <nicholasSewing@gmail.com>
2022-09-02 13:36:32 -0400

    add node modules to externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const path = require("path");
 const fs = require("fs");
 
+const externals = ["pg-hstore"];
+externals.push(
+  fs.readdirSync("node_modules").filter((x) => {
+    return x !== ".bin";
+  })
+);
+
 module.exports = {
   entry: "./src/lambda.js",
   target: "node",
@@ -12,10 +19,7 @@ module.exports = {
     // library: 'serverlessExpressEdge',
     libraryTarget: "commonjs2",
   },
-  externals: ["pg-hstore"],
-  // externals: fs.readdirSync("node_modules").filter((x) => {
-  //   return x !== ".bin";
-  // }),
+  externals,
   // optimization: {
   //   concatenateModules: false,
   // },
