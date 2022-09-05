8cb450ddc75b26f42e9a55e62c31dc72fe2726dd
Nicholas Ewing <nicholasSewing@gmail.com>
2022-09-05 17:45:44 -0400

    Updates layout / tailwind config, deletes jest tests

--- jest/makeTables.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require("../sequelize/index");
-
-const makeTables = async () => {
-  if (process.env.NODE_ENV === "test") {
-    await sequelize.sync({ force: true });
-  }
-};
-
-module.exports = makeTables;
