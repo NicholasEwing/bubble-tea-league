c9f4f9536820c568cb96614ee32ec43ad567bdd0
Nicholas Ewing <nicholasSewing@gmail.com>
2022-09-07 19:10:52 -0400

    Fix /api/matches and /api/players

--- pages/api/matches/index.js
+++ pages/api/matches/index.js
@@ -1,75 +1,47 @@
-// import { generateTournamentCodes } from "../../../lib/riot-games-api-helpers";
-// import { unstable_getServerSession } from "next-auth/next";
-// import { authOptions } from "../auth/[...nextauth]";
-// import admins from "../../../sequelize/admins";
+import { generateTournamentCodes } from "../../../lib/riot-games-api-helpers";
+import { unstable_getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
+import admins from "../../../admins";
 
-// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// const sequelize = require("../../../sequelize");
-// const { Match, MatchRound, Season } = sequelize.models;
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-// export default async function handler(req, res) {
-//   try {
-//     if (process.env.NODE_ENV === "production") {
-//       const session = await unstable_getServerSession(req, res, authOptions);
-//       const userIsAdmin = admins.includes(session?.user?.email);
-//       if (!userIsAdmin) res.status(401).end();
-//     }
-//     switch (req.method) {
-//       // case "POST":
-//       //   const { season, bestOf } = req.body;
-//       //   const match = await Match.create({
-//       //     season,
-//       //     isPlayoffsMatch: bestOf === 3,
-//       //   });
+const { prisma } = require("../../../prisma/db");
 
-//       //   const MatchId = match.dataValues.id;
-//       //   const metadata = {
-//       //     MatchId,
-//       //     riotAuth: process.env.BTL_API_KEY,
-//       //   };
+export default async function handler(req, res) {
+  try {
+    if (process.env.NODE_ENV === "production") {
+      const session = await unstable_getServerSession(req, res, authOptions);
+      const userIsAdmin = admins.includes(session?.user?.email);
+      if (!userIsAdmin) res.status(401).end();
+    }
+    switch (req.method) {
+      case "GET":
+        const matches = await Match.findAll();
+        res.status(200).json(matches);
+        break;
+      case "PATCH":
+        try {
+          const { matches } = req.body;
+          for (const match of matches) {
+            const { scheduledTime, vodLink, id } = match;
 
-//       //   const { tournamentId } = await Season.findByPk(season);
-
-//       //   // generate X number of tournament codes via Riot Games API
-//       //   const tournamentCodes = await generateTournamentCodes(
-//       //     bestOf,
-//       //     tournamentId,
-//       //     [MatchId]
-//       //   );
-
-//       //   // For every code made, make a match round record and slap a tourny code on it
-//       //   tournamentCodes.forEach(async (tournamentCode) => {
-//       //     try {
-//       //       const matchRoundObj = {
-//       //         MatchId,
-//       //         tournamentCode,
-//       //       };
-
-//       //       await MatchRound.create(matchRoundObj);
-//       //     } catch (error) {
-//       //       console.error("Error inside tourny code loop:", error);
-//       //     }
-//       //   });
-
-//       //   res.status(201).end();
-//       //   break;
-//       case "GET":
-//         const matches = await Match.findAll();
-//         res.status(200).json(matches);
-//         break;
-//       case "PATCH":
-//         try {
-//           const { matches } = req.body;
-//           await Match.bulkCreate(matches, {
-//             updateOnDuplicate: ["scheduledTime", "vodLink"],
-//           });
-//           res.status(200).send();
-//         } catch (error) {
-//           res.status(500).send({ error });
-//         }
-//         break;
-//     }
-//   } catch (error) {
-//     console.error("Error inside /api/matches", error);
-//   }
-// }
+            await prisma.match.update({
+              where: {
+                id,
+              },
+              data: {
+                scheduledTime: new Date(scheduledTime),
+                vodLink,
+              },
+            });
+          }
+          res.status(200).send();
+        } catch (error) {
+          res.status(500).send(error);
+        }
+        break;
+    }
+  } catch (error) {
+    console.error("Error inside /api/matches", error);
+  }
+}
--- pages/api/players/index.js
+++ pages/api/players/index.js
@@ -1,82 +1,89 @@
-// import { getPlayerPUUID } from "../../../lib/riot-games-api-helpers";
-// import { unstable_getServerSession } from "next-auth/next";
-// import { authOptions } from "../auth/[...nextauth]";
-// import admins from "../../../sequelize/admins";
-// import { isValidEmail } from "../../../lib/utils";
+import { getPlayerPUUID } from "../../../lib/riot-games-api-helpers";
+import { unstable_getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
+import { isValidEmail } from "../../../lib/utils";
+import admins from "../../../admins";
 
-// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// const sequelize = require("../../../sequelize");
-// const { Player } = sequelize.models;
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { prisma } = require("../../../prisma/db");
 
-// export default async function handler(req, res) {
-//   try {
-//     if (process.env.NODE_ENV === "production") {
-//       const session = await unstable_getServerSession(req, res, authOptions);
-//       const userIsAdmin = admins.includes(session?.user?.email);
-//       if (!userIsAdmin) res.status(401).end();
-//     }
+export default async function handler(req, res) {
+  try {
+    if (process.env.NODE_ENV === "production") {
+      const session = await unstable_getServerSession(req, res, authOptions);
+      const userIsAdmin = admins.includes(session?.user?.email);
+      if (!userIsAdmin) res.status(401).end();
+    }
 
-//     switch (req.method) {
-//       case "POST":
-//         try {
-//           // if no PUUID given...
-//           const { summonerName, discordName, firstName, email } = req.body;
-//           let { PUUID } = req.body;
-//           if (!PUUID) PUUID = await getPlayerPUUID(summonerName);
-//           const { id } = await Player.create({
-//             PUUID,
-//             summonerName,
-//             discordName,
-//             firstName,
-//             email: isValidEmail(email) ? email : null,
-//           });
-//           res.status(201).send({ id });
-//         } catch (error) {
-//           let message;
+    switch (req.method) {
+      case "POST":
+        try {
+          const { summonerName, discordName, firstName, email, isFreeAgent } =
+            req.body;
+          let { PUUID } = req.body;
+          if (!PUUID) PUUID = await getPlayerPUUID(summonerName);
+          await prisma.player.create({
+            data: {
+              PUUID,
+              summonerName,
+              discordName,
+              firstName,
+              isFreeAgent,
+              email: isValidEmail(email) ? email : null,
+            },
+          });
+          res.status(201).end();
+        } catch (error) {
+          res.status(409).json(error);
+        }
+        break;
+      case "GET":
+        const players = await Player.findAll();
+        res.status(200).json(players);
+        break;
+      case "PATCH":
+        try {
+          const { players } = req.body;
+          for (const player of players) {
+            const { id, summonerName, firstName, discordName, role, email } =
+              player;
 
-//           if (error?.parent?.sqlMessage) {
-//             message = error.parent.sqlMessage;
-//           } else {
-//             message = error.message;
-//           }
-
-//           res.status(409).send({ message });
-//         }
-//         break;
-//       case "GET":
-//         const players = await Player.findAll();
-//         res.status(200).json(players);
-//         break;
-//       case "PATCH":
-//         try {
-//           const { players } = req.body;
-//           await Player.bulkCreate(players, {
-//             updateOnDuplicate: [
-//               "summonerName",
-//               "firstName",
-//               "discordName",
-//               "role",
-//               "email",
-//             ],
-//           });
-//           res.status(200).send();
-//         } catch (error) {
-//           res.status(500).send({ error });
-//         }
-//         break;
-//       case "DELETE":
-//         try {
-//           const { idsToDelete } = req.body;
-//           await Player.destroy({
-//             where: { id: idsToDelete },
-//           });
-//           res.status(200).send();
-//         } catch (error) {
-//           res.status(404).send({ error });
-//         }
-//         break;
-//     }
-//   } catch (error) {
-//     console.error("Error inside /api/players", error);
-//   }
-// }
+            await prisma.player.update({
+              where: {
+                id,
+              },
+              data: {
+                summonerName,
+                firstName,
+                discordName,
+                role,
+                email,
+              },
+            });
+          }
+          res.status(200).end();
+        } catch (error) {
+          res.status(500).send({ error });
+        }
+        break;
+      case "DELETE":
+        try {
+          const { idsToDelete } = req.body;
+          await prisma.player.deleteMany({
+            where: {
+              id: {
+                in: idsToDelete,
+              },
+            },
+          });
+          res.status(200).end();
+        } catch (error) {
+          console.log("error", error);
+          res.status(404).send({ error });
+        }
+        break;
+    }
+  } catch (error) {
+    console.error("Error inside /api/players", error);
+  }
+}
