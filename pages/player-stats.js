f32277c7c5471e2d821eb4114c94d1dc936a4fdc
Nicholas Ewing <nicholasSewing@gmail.com>
2022-09-09 17:51:41 -0400

    Displays player-stats page again with Prisma

--- pages/player-stats.js
+++ pages/player-stats.js
@@ -19,7 +19,7 @@ export const getStaticProps = async () => {
 
   // calculates the avg of anything per game for a player, (avg damage, avg cs, avg deaths, etc)
   function calculateStatPerMin(
-    PlayerId,
+    playerId,
     playerTeamMatchRounds,
     playerMatches,
     statKey,
@@ -30,7 +30,7 @@ export const getStaticProps = async () => {
 
       const matchRoundId = mr.id;
       const playerMatch = playerMatches.find(
-        (m) => m.playerId === playerId && m.matchRoundId === matchRoundId
+        (m) => m.playerId == playerId && m.matchRoundId == matchRoundId
       );
       const gameStat = playerMatch[statKey];
 
@@ -52,179 +52,185 @@ export const getStaticProps = async () => {
   }
 
   const seasonRows = [];
-  for (const season of seasons) {
-    const seasonRow = [];
+  try {
+    for (const season of seasons) {
+      const seasonRow = [];
 
-    // parse a player's seasonal stats in a big object
-    for (const history of playerTeamHistories) {
-      const { playerId, teamId, role } = history;
-      const player = players.find((p) => p.id === playerId);
-      const team = findTeamName(teamId, teams);
-      const { summonerName } = player;
+      // parse a player's seasonal stats in a big object
+      for (const history of playerTeamHistories) {
+        const { playerId, teamId, role } = history;
+        const player = players.find((p) => p.id == playerId);
+        const team = findTeamName(teamId, teams);
+        const { summonerName } = player;
 
-      // find all finished match ids for the season with this player's team in it
-      const seasonMatches = matches.filter(
-        (m) =>
-          m.season === season.id &&
-          m.matchWinnerTeamId &&
-          (m.teamOne === teamId || m.teamTwo === teamId)
-      );
+        // find all finished match ids for the season with this player's team in it
+        const seasonMatches = matches.filter(
+          (m) =>
+            m.seasonId == season.id &&
+            m.matchWinnerTeamId &&
+            (m.teamOneId == teamId || m.teamTwoId == teamId)
+        );
 
-      // no matches yet? skip this player
-      if (!seasonMatches.length) break;
+        // no matches yet? skip this player
+        if (!seasonMatches.length) break;
 
-      const seasonMatchIds = seasonMatches.map((m) => m.id);
-      const seasonMatchRounds = matchRounds.filter((mr) =>
-        seasonMatchIds.includes(mr.matchId)
-      );
-      const seasonMatchRoundIds = seasonMatchRounds.map((mr) => mr.id);
+        const seasonMatchIds = seasonMatches.map((m) => m.id);
+        const seasonMatchRounds = matchRounds.filter((mr) =>
+          seasonMatchIds.includes(mr.matchId)
+        );
+        const seasonMatchRoundIds = seasonMatchRounds.map((mr) => mr.id);
 
-      // all match rounds with this player's team involved that have a winner
-      const playerTeamMatchRounds = matchRounds.filter(
-        (mr) =>
-          (mr.redTeamId === teamId || mr.blueTeamId === teamId) &&
-          mr.winningTeamId
-      );
+        // all match rounds with this player's team involved that have a winner
+        const playerTeamMatchRounds = matchRounds.filter(
+          (mr) =>
+            (mr.redTeamId == teamId || mr.blueTeamId == teamId) &&
+            mr.winningTeamId
+        );
 
-      // filter down to stat records for this player and their team
-      const playerMatches = matchRoundPlayerStats.filter(
-        (mrps) =>
-          seasonMatchRoundIds.includes(mrps.matchRoundId) &&
-          mrps.playerId === playerId
-      );
-      const teamMatches = matchRoundTeamStats.filter(
-        (mrts) => mrts.teamId === teamId
-      );
+        // console.log("player stats", matchRoundPlayerStats);
+        // filter down to stat records for this player and their team
+        const playerMatches = matchRoundPlayerStats.filter(
+          (mrps) =>
+            seasonMatchRoundIds.includes(mrps.matchRoundId) &&
+            mrps.playerId == playerId
+        );
 
-      const kills = playerMatches.reduce(
-        (kills, stats) => (kills += stats.kills),
-        0
-      );
-      const deaths = playerMatches.reduce(
-        (deaths, stats) => (deaths += stats.deaths),
-        0
-      );
-      const assists = playerMatches.reduce(
-        (assists, stats) => (assists += stats.assists),
-        0
-      );
-      const totalCs = playerMatches
-        .reduce(
-          (totalMinionsKilled, stats) =>
-            (totalMinionsKilled += stats.totalMinionsKilled),
+        const teamMatches = matchRoundTeamStats.filter(
+          (mrts) => mrts.teamId == teamId
+        );
+
+        const kills = playerMatches.reduce(
+          (kills, stats) => (kills += stats.kills),
           0
-        )
-        .toLocaleString("en-US");
-      const gamesPlayed = playerMatches.length;
-      const averageGameTimeSeconds =
-        playerTeamMatchRounds.reduce(
-          (time, match) => (time += match.gameDuration),
+        );
+        const deaths = playerMatches.reduce(
+          (deaths, stats) => (deaths += stats.deaths),
           0
-        ) / gamesPlayed;
-      const minutes = Math.floor(averageGameTimeSeconds / 60);
-      const seconds = Math.floor(averageGameTimeSeconds % 60);
-
-      const averageGameTime = `${padTo2Digits(minutes)}:${padTo2Digits(
-        seconds
-      )}`;
-
-      const creepsPerMin = calculateStatPerMin(
-        PlayerId,
-        playerTeamMatchRounds,
-        playerMatches,
-        "totalMinionsKilled",
-        gamesPlayed
-      );
-
-      const totalGold = playerMatches
-        .reduce((gold, stats) => (gold += stats.goldEarned), 0)
-        .toLocaleString("en-US");
-
-      const goldPerMin = calculateStatPerMin(
-        playerId,
-        playerTeamMatchRounds,
-        playerMatches,
-        "goldEarned",
-        gamesPlayed
-      );
-      const visionScore = playerMatches.reduce(
-        (vs, stats) => (vs += stats.visionScore),
-        0
-      );
-      const visionScorePerGame =
-        Math.round((visionScore / gamesPlayed) * 100) / 100;
-      const visionScorePerMin = calculateStatPerMin(
-        playerId,
-        playerTeamMatchRounds,
-        playerMatches,
-        "visionScore",
-        gamesPlayed
-      );
-      const avgTeamPercentDmg =
-        Math.round(
-          (playerMatches.reduce((teamPercentDmg, match) => {
-            return (teamPercentDmg += parseInt(match.teamDamagePercentage));
-          }, 0) /
-            gamesPlayed) *
-            100
-        ) / 100;
-
-      const kda = playerMatches.reduce((kda, stats) => (kda += stats.kda), 0);
-
-      const killParticipationPercent = percentageFormatter(
-        (kills + assists) /
-          teamMatches.reduce(
-            (teamKills, match) => (teamKills += match.kills),
+        );
+        const assists = playerMatches.reduce(
+          (assists, stats) => (assists += stats.assists),
+          0
+        );
+        const totalCs = playerMatches
+          .reduce(
+            (totalMinionsKilled, stats) =>
+              (totalMinionsKilled += stats.totalMinionsKilled),
             0
           )
-      );
-      const totalDmgToChamps = playerMatches
-        .reduce((dmg, stats) => (dmg += stats.totalDmgToChamps), 0)
-        .toLocaleString("en-US");
-      const dmgToChampsPerMin = calculateStatPerMin(
-        playerId,
-        playerTeamMatchRounds,
-        playerMatches,
-        "totalDmgToChamps",
-        gamesPlayed
-      );
+          .toLocaleString("en-US");
+        const gamesPlayed = playerMatches.length;
+        const averageGameTimeSeconds =
+          playerTeamMatchRounds.reduce(
+            (time, match) => (time += match.gameDuration),
+            0
+          ) / gamesPlayed;
+        const minutes = Math.floor(averageGameTimeSeconds / 60);
+        const seconds = Math.floor(averageGameTimeSeconds % 60);
 
-      const firstBloods = playerMatches.reduce(
-        (fbs, stats) => (fbs += stats.firstBlood),
-        0
-      );
+        const averageGameTime = `${padTo2Digits(minutes)}:${padTo2Digits(
+          seconds
+        )}`;
 
-      const playerStatRow = {
-        season: season.id,
-        summonerName,
-        role,
-        team,
-        kills,
-        deaths,
-        assists,
-        totalCs,
-        gamesPlayed,
-        averageGameTime,
-        totalDmgToChamps,
-        creepsPerMin,
-        totalGold,
-        dmgToChampsPerMin,
-        avgTeamPercentDmg,
-        visionScore,
-        visionScorePerGame,
-        visionScorePerMin,
-        killParticipationPercent,
-        goldPerMin,
-        kda,
-        firstBloods,
-      };
+        const creepsPerMin = calculateStatPerMin(
+          playerId,
+          playerTeamMatchRounds,
+          playerMatches,
+          "totalMinionsKilled",
+          gamesPlayed
+        );
 
-      // each item in here represent's a player's stat (or one row on the table)
-      seasonRow.push(playerStatRow);
+        const totalGold = playerMatches
+          .reduce((gold, stats) => (gold += stats.goldEarned), 0)
+          .toLocaleString("en-US");
+
+        const goldPerMin = calculateStatPerMin(
+          playerId,
+          playerTeamMatchRounds,
+          playerMatches,
+          "goldEarned",
+          gamesPlayed
+        );
+        const visionScore = playerMatches.reduce(
+          (vs, stats) => (vs += stats.visionScore),
+          0
+        );
+        const visionScorePerGame =
+          Math.round((visionScore / gamesPlayed) * 100) / 100;
+        const visionScorePerMin = calculateStatPerMin(
+          playerId,
+          playerTeamMatchRounds,
+          playerMatches,
+          "visionScore",
+          gamesPlayed
+        );
+        const avgTeamPercentDmg =
+          Math.round(
+            (playerMatches.reduce((teamPercentDmg, match) => {
+              return (teamPercentDmg += parseInt(match.teamDamagePercentage));
+            }, 0) /
+              gamesPlayed) *
+              100
+          ) / 100;
+
+        const kda = playerMatches.reduce((kda, stats) => (kda += stats.kda), 0);
+
+        const killParticipationPercent = percentageFormatter(
+          (kills + assists) /
+            teamMatches.reduce(
+              (teamKills, match) => (teamKills += match.kills),
+              0
+            )
+        );
+        const totalDmgToChamps = playerMatches
+          .reduce((dmg, stats) => (dmg += stats.totalDmgToChamps), 0)
+          .toLocaleString("en-US");
+        const dmgToChampsPerMin = calculateStatPerMin(
+          playerId,
+          playerTeamMatchRounds,
+          playerMatches,
+          "totalDmgToChamps",
+          gamesPlayed
+        );
+
+        const firstBloods = playerMatches.reduce(
+          (fbs, stats) => (fbs += stats.firstBlood),
+          0
+        );
+
+        const playerStatRow = {
+          seasonId: season.id,
+          summonerName,
+          role,
+          team,
+          kills,
+          deaths,
+          assists,
+          totalCs,
+          gamesPlayed,
+          averageGameTime,
+          totalDmgToChamps,
+          creepsPerMin,
+          totalGold,
+          dmgToChampsPerMin,
+          avgTeamPercentDmg,
+          visionScore,
+          visionScorePerGame,
+          visionScorePerMin,
+          killParticipationPercent,
+          goldPerMin,
+          kda,
+          firstBloods,
+        };
+
+        // each item in here represent's a player's stat (or one row on the table)
+        seasonRow.push(playerStatRow);
+      }
+
+      // each item in here represents a season
+      seasonRows.push(seasonRow);
     }
-
-    // each item in here represents a season
-    seasonRows.push(seasonRow);
+  } catch (error) {
+    console.log("error", error);
   }
 
   return {
@@ -236,7 +242,7 @@ export const getStaticProps = async () => {
 };
 
 export default function PlayerStats({ seasons = null, seasonRows = null }) {
-  const [activeSeason, setActiveSeason] = useState(seasons[0]?.number || 1);
+  const [activeSeason, setActiveSeason] = useState(seasons[0]?.id || 1);
 
   const rowsToDisplay = seasonRows[activeSeason - 1] || [];
 
