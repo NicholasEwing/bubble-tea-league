d604081ec570106e3e0f716732b3bc0ffdc1d098
Nicholas Ewing <nicholasSewing@gmail.com>
2022-09-21 19:39:26 -0400

    Add placeholder YT vod for demo purposes

--- components/home/HomeContent/index.js
+++ components/home/HomeContent/index.js
@@ -7,11 +7,16 @@ export default function HomeContent({
   bestOf,
   scheduledTime,
   season,
+  featuredMatch,
 }) {
   return (
     <div className="HomeLiveBanner relative bg-home-hero bg-cover py-10">
       <a
-        href="https://www.twitch.tv/thebubbletealeague"
+        href={
+          featuredMatch
+            ? "https://www.twitch.tv/thebubbletealeague"
+            : "https://www.youtube.com/channel/UCp9thtIHYrohmAW3qzQCLfQ"
+        }
         className="invisible-overlay-button z-10 h-full w-full cursor-pointer"
       >
         <div className="header mb-5 flex flex-row items-center justify-center">
@@ -23,14 +28,14 @@ export default function HomeContent({
           />
           <div className="text align-center relative mb-5 flex flex-col justify-center text-center text-white">
             <div className="league-round text-sm font-medium uppercase tracking-widest opacity-60 md:text-base">
-              <span className="name">Season {season}</span>
+              <span className="name">Season {season || "8"}</span>
               <span className="hyphen">&nbsp;-&nbsp;</span>
               <span className="round">{bestOf}</span>
             </div>
             <div className="title text-xl font-bold md:text-5xl">
-              {teamOne.tricode}
+              {teamOne?.tricode || "BEE"}
               <span className="vs lower px-4 text-sm md:text-3xl">VS</span>
-              {teamTwo.tricode}
+              {teamTwo?.tricode || "AR"}
             </div>
           </div>
           <TeamLogo
@@ -41,17 +46,30 @@ export default function HomeContent({
           />
         </div>
         <div className="video-container pointer-events-none relative hidden min-h-[60px] flex-col items-center md:flex">
-          <iframe
-            title="Proving Grounds - TL VS 100"
-            className="video"
-            height="410"
-            width="730"
-            src="https://player.twitch.tv/?channel=thebubbletealeague&amp;autoplay=true&amp;muted=true&amp;controls=false&amp;volume=0&amp;parent=localhost:3000"
-            frameBorder="0"
-            allow="autoplay"
-          ></iframe>
-          <div className="watch-live absolute bottom-5 left-1/2 -translate-x-1/2 rounded-3xl bg-[#de2f2f] py-3 px-6 font-medium tracking-widest text-white">
-            WATCH LIVE
+          {featuredMatch ? (
+            <iframe
+              title="Proving Grounds - TL VS 100"
+              className="video"
+              height="410"
+              width="730"
+              src="https://player.twitch.tv/?channel=thebubbletealeague&amp;autoplay=true&amp;muted=true&amp;controls=false&amp;volume=0&amp;parent=localhost:3000"
+              frameBorder="0"
+              allow="autoplay"
+            ></iframe>
+          ) : (
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/8g1OcOlQTZY?start=1365&amp;autoplay=1&amp;mute=1"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          )}
+
+          <div className="watch-live absolute bottom-5 left-1/2 -translate-x-1/2 rounded-3xl bg-[#de2f2f] py-3 px-6 font-medium uppercase tracking-widest text-white">
+            {featuredMatch ? "Watch Live" : "Watch VODs"}
           </div>
         </div>
       </a>
