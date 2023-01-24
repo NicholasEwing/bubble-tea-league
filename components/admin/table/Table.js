ae34d7e88c29a398cfbd074c90284ae25e724f7d
Nicholas Ewing <nicholasSewing@gmail.com>
2023-01-24 13:19:59 -0500

    Deletes old EditableTable components

--- components/admin/table/Table.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Table({ children }) {
-  return (
-    <table className="min-w-full table-fixed divide-y divide-gray-600">
-      {children}
-    </table>
-  );
-}
--- components/admin/table/TableBody.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function TableBody({ children }) {
-  return (
-    <tbody className="divide-y divide-gray-700 bg-gray-800">{children}</tbody>
-  );
-}
